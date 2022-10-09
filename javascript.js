37153daec1517ab6ee89939cdcead0e4ae5f786b
EsmeraldaTorres <estorresag2019@gmail.com>
2022-10-09 14:44:17 -0500

    modificaciones cliente

--- javascript.js
+++ javascript.js
@@ -36,7 +36,7 @@ playPause.addEventListener("click", () => {
 
 // a regresiva -------------------------------------------------------------------
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 29, 2022 14:00").getTime();
+var countDownDate = new Date("Oct 29, 2022 14:30").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
